f41336c35e927765bc3f8c31ed6f2a271dda1048
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2016-11-26 11:27:55 -0800

    set up vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import store from './vuex/store.js'
+
 new Vue({
+  // This will inject the store instance to all child components who access it with: this.$store.
+  store,
   el: '#app',
   render: h => h(App)
 })
