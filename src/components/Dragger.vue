3df1ffb1488190be95f633b9fd6d708a29aa4f09
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2016-11-25 21:39:07 -0800

    Temp Dragger component for testing

---
+++ src/components/Dragger.vue
@@ -0,0 +1,20 @@
+<template>
+  <img src="../assets/logo.png" draggable="false">
+</template>
+
+<script>
+import $ from 'jquery';
+
+export default {
+  created: onCreate
+}
+
+function onCreate() {
+  console.log('oh really');
+}
+
+</script>
+
+
+<style>
+</style>
